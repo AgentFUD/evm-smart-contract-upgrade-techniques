2ec8eec1936eadb9d70a00885c71ca2b033f835f
Agent FUD <agentfud@gmail.com>
2021-11-19 02:58:50 +0100

    code formatted

--- eternal-storage/tests/conftest.py
+++ eternal-storage/tests/conftest.py
@@ -5,20 +5,20 @@ from brownie import EternalStorage, SocialLib, SocialSite, SocialLibV2, SocialSi
 @pytest.fixture(scope="module")
 def social_site_contract(accounts):
     owner = accounts[0]
-    es = EternalStorage.deploy({'from': owner})
-    SocialLib.deploy({'from': owner})
-    ss = SocialSite.deploy(es.address, {'from': owner})
+    es = EternalStorage.deploy({"from": owner})
+    SocialLib.deploy({"from": owner})
+    ss = SocialSite.deploy(es.address, {"from": owner})
     return ss
+
 
 @pytest.fixture(scope="module")
 def social_site_contract_v2(accounts):
     owner = accounts[0]
-    es = EternalStorage.deploy({'from': owner})
+    es = EternalStorage.deploy({"from": owner})
 
-    SocialLib.deploy({'from': owner})
-    ssv1 = SocialSite.deploy(es.address, {'from': owner})
+    SocialLib.deploy({"from": owner})
+    ssv1 = SocialSite.deploy(es.address, {"from": owner})
 
-    SocialLibV2.deploy({'from': owner})
-    ssv2 = SocialSiteV2.deploy(es.address, {'from': owner})
+    SocialLibV2.deploy({"from": owner})
+    ssv2 = SocialSiteV2.deploy(es.address, {"from": owner})
     return ssv1, ssv2
-
--- eternal-storage/tests/test_social_site.py
+++ eternal-storage/tests/test_social_site.py
@@ -1,72 +1,77 @@
 import brownie
 
-def test_we_can_like_an_url(social_site_contract):
-    social_site_contract.like('https://orno.io')
-    social_site_contract.like('https://orno.io')
-    social_site_contract.like('https://orno.io')
-    social_site_contract.like('https://orno.io')
 
-    num_likes = social_site_contract.getLikes('https://orno.io')
+def test_we_can_like_an_url(social_site_contract):
+    social_site_contract.like("https://orno.io")
+    social_site_contract.like("https://orno.io")
+    social_site_contract.like("https://orno.io")
+    social_site_contract.like("https://orno.io")
+
+    num_likes = social_site_contract.getLikes("https://orno.io")
 
     assert num_likes == 4
 
-    assert social_site_contract.getLikes('https://youtube.com') == 0
+    assert social_site_contract.getLikes("https://youtube.com") == 0
+
 
 def test_we_can_dislike_an_url(social_site_contract):
-    social_site_contract.disLike('https://youtube.com')
-    social_site_contract.disLike('https://youtube.com')
-    social_site_contract.disLike('https://youtube.com')
-    
-    assert social_site_contract.getDisLikes('https://youtube.com') == 3
+    social_site_contract.disLike("https://youtube.com")
+    social_site_contract.disLike("https://youtube.com")
+    social_site_contract.disLike("https://youtube.com")
+
+    assert social_site_contract.getDisLikes("https://youtube.com") == 3
 
 
 def test_we_can_mix_likes_and_dislikes(social_site_contract):
-    likes = social_site_contract.getLikes('https://orno.io')
-    disLikes = social_site_contract.getDisLikes('https://orno.io')
-    
-    social_site_contract.like('https://orno.io')
-    social_site_contract.like('https://orno.io')
-    social_site_contract.disLike('https://orno.io')
-    social_site_contract.disLike('https://orno.io')
-    social_site_contract.like('https://orno.io')
-    social_site_contract.like('https://orno.io')
-    
-    assert social_site_contract.getLikes('https://orno.io') == likes + 4
-    assert social_site_contract.getDisLikes('https://orno.io') == disLikes + 2
-    
+    likes = social_site_contract.getLikes("https://orno.io")
+    disLikes = social_site_contract.getDisLikes("https://orno.io")
+
+    social_site_contract.like("https://orno.io")
+    social_site_contract.like("https://orno.io")
+    social_site_contract.disLike("https://orno.io")
+    social_site_contract.disLike("https://orno.io")
+    social_site_contract.like("https://orno.io")
+    social_site_contract.like("https://orno.io")
+
+    assert social_site_contract.getLikes("https://orno.io") == likes + 4
+    assert social_site_contract.getDisLikes("https://orno.io") == disLikes + 2
+
+
 def test_storage_keeps_values_if_switched_to_v2(social_site_contract_v2):
     orig = social_site_contract_v2[0]
     updated = social_site_contract_v2[1]
 
-    orig.like('https://orno.io/article')
-    orig.disLike('https://myblog.io/article')
+    orig.like("https://orno.io/article")
+    orig.disLike("https://myblog.io/article")
 
-    assert orig.getLikes('https://orno.io/article') == 1
-    assert updated.getLikes('https://orno.io/article') == 1
+    assert orig.getLikes("https://orno.io/article") == 1
+    assert updated.getLikes("https://orno.io/article") == 1
 
-    assert orig.getDisLikes('https://myblog.io/article') == 1
-    assert updated.getDisLikes('https://myblog.io/article') == 1
+    assert orig.getDisLikes("https://myblog.io/article") == 1
+    assert updated.getDisLikes("https://myblog.io/article") == 1
+
 
 def test_v2_only_allows_like_once(social_site_contract_v2):
     orig = social_site_contract_v2[0]
     updated = social_site_contract_v2[1]
 
-    updated.like('https://orno.io/article')
+    updated.like("https://orno.io/article")
 
-    assert orig.getLikes('https://orno.io/article') == 2
-    assert updated.getLikes('https://orno.io/article') == 2
+    assert orig.getLikes("https://orno.io/article") == 2
+    assert updated.getLikes("https://orno.io/article") == 2
 
     with brownie.reverts("ERROR: User has already liked"):
-        updated.like('https://orno.io/article')
+        updated.like("https://orno.io/article")
+
 
 def test_v2_only_allows_dislike_once(social_site_contract_v2):
     orig = social_site_contract_v2[0]
     updated = social_site_contract_v2[1]
 
-    assert orig.getDisLikes('https://myblog.io/article') == 1
-    assert updated.getDisLikes('https://myblog.io/article') == 1
+    assert orig.getDisLikes("https://myblog.io/article") == 1
+    assert updated.getDisLikes("https://myblog.io/article") == 1
 
-    updated.disLike('https://myblog.io/article')
-    
+    updated.disLike("https://myblog.io/article")
+
     with brownie.reverts("ERROR: User has already disliked"):
-        updated.disLike('https://myblog.io/article')
+        updated.disLike("https://myblog.io/article")
