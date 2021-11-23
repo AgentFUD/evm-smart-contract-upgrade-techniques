061efeb38b75f3c75ae0ac27a6d347a6585c1007
Agent FUD <agentfud@gmail.com>
2021-11-23 13:10:25 +0100

    code reformatted

--- ancient-proxy/tests/conftest.py
+++ ancient-proxy/tests/conftest.py
@@ -7,9 +7,8 @@ from brownie import Dispatcher, BusinessLogicV1
 @pytest.fixture(scope="module")
 def bl_v1(accounts):
     owner = accounts[0]
-    blv1 = BusinessLogicV1.deploy({'from': owner})
-    disp = Dispatcher.deploy(blv1.address, {'from': owner})
+    blv1 = BusinessLogicV1.deploy({"from": owner})
+    disp = Dispatcher.deploy(blv1.address, {"from": owner})
 
     # blv1 runs on disp address
     return Contract.from_abi("BusinessLogicV1", disp.address, blv1.abi)
-
